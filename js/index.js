f9742a9795e84e4a1aea68eb0b7cc05b9f6e1e00
Aliaksei Karzhou <alexius90e@gmail.com>
2025-04-08 17:48:30 +0300

    feat: added header menu

--- js/index.js
+++ js/index.js
@@ -1,6 +1,19 @@
 const headerBurgerButton = document.querySelector('.header__burger-button');
+const headerNav = document.querySelector('.header__nav');
 const headerSearchButton = document.querySelector('.header__search-button');
 const headerSearchbar = document.querySelector('.header__searchbar');
+
+if (headerNav && headerBurgerButton) {
+  headerBurgerButton.addEventListener('click', () => {
+    headerNav.classList.toggle('active');
+  });
+
+  headerNav.addEventListener('click', (event) => {
+    if (event.target === event.currentTarget) {
+      event.currentTarget.classList.remove('active');
+    }
+  });
+}
 
 if (headerSearchButton && headerSearchbar) {
   headerSearchButton.addEventListener('click', () => {
@@ -13,3 +26,13 @@ if (headerSearchButton && headerSearchbar) {
     }
   });
 }
+
+const headerNavMenuItems = document.querySelectorAll('.header__nav-menu-item');
+
+headerNavMenuItems.forEach((item) => {
+  item.addEventListener('click', (event) => {
+    const isLayout = event.target === event.currentTarget;
+
+    if (isLayout) event.currentTarget.classList.toggle('active');
+  });
+});
